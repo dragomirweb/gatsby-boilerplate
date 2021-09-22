88a37adf50e4bbff0dd657dc8836718e7826014e
Daniel <daniel@adhub.dk>
2021-09-22 19:39:45 +0300

    add xstyled sanity layout

---
+++ src/layouts/StandardLayout.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import {
+  defaultTheme,
+  ThemeProvider,
+  Preflight,
+} from '@xstyled/styled-components'
+
+import '../assets/css/reset.css'
+
+const theme = {
+  ...defaultTheme,
+}
+
+const StandardLayout = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <Preflight />
+    {children}
+  </ThemeProvider>
+)
+
+export default StandardLayout
